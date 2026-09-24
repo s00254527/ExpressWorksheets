8e159381066581d631d4a1dbce4f5c7fbbc609af
NotBrian122 <s00254527@atu.ie>
2026-09-24 14:44:23 +0100

    worksheet 3 done?

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, {Application, Request, Response} from "express" ; 
+import express, {Application} from "express" ; 
 import {env} from "./config/env";
 import { connectDB } from "./database/db";
 import carRoutes from "../src/routes/cars";
